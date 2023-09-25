ed8bf84163bee63cb222f6383a62eb9391631c7f
TitadachFF <titadach15632441@gmail.com>
2023-09-25 16:14:30 +0700

    first

---
+++ src/CashPayment.java
@@ -0,0 +1,10 @@
+
+public class CashPayment implements PaymentStrategy {
+
+	@Override
+	public void pay(int amount) {
+		// TODO Auto-generated method stub
+		System.out.println("Paid by cash: " + amount);
+	}
+
+}
---
+++ src/CreditCardPayment.java
@@ -0,0 +1,11 @@
+
+public class CreditCardPayment implements PaymentStrategy {
+
+
+	@Override
+	public void pay(int amount) {
+		// TODO Auto-generated method stub
+		System.out.println("Paid by credit card: " + amount);
+	}
+
+}
